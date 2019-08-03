7a4cfc817e7ae09cb72331801962dfea1b9b7ca6
Jens von Pilgrim <developer@jevopi.de>
2019-08-03 15:14:03 +0200

    Test7

--- src/test/java/lift/SimpleLiftTest.java
+++ src/test/java/lift/SimpleLiftTest.java
@@ -10,7 +10,7 @@ class SimpleLiftTest {
 
 	@Test
 	void test() {
-		System.out.println("Test 6");
+		System.out.println("Test 7");
 		main.start();
 		System.out.println(main);
 		for (int i = 0; i < 60; i++) {
